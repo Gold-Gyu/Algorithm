677317db226caa643fa4e01cdbb94bb2f119fe41
Gold-gyu <lkg2097@naver.com>
2023-02-28 23:45:26 +0900

    0228

---
+++ IM/2차행렬/2. 오셀로.py
@@ -0,0 +1,41 @@
+# 파리퇴치
+# sum
+# 인덱스 일부에 접근하고
+# 최대최소랑 비교하기
+# 델타를 써서 확장해가는 풍선팡, 오목
+# black = 1
+# white = 2
+# OP = [0, 2, 1]
+#
+# T = int(input())
+#
+# for tc in range(1, T+1):
+#     N, M = map(int, input().split())
+#     board = [[0] * (N+2) for _ in range(N+2)]
+#     board[N//2][N//2] = white       # 중앙지점
+#     board[N // 2][N // 2 + 1] = black
+#     board[N // 2 + 1][N // 2] = black
+#     board[N // 2 + 1][N // 2 + 1] = white
+#     for _ in range(M):
+#         j, i, bw = map(int, input().split())
+#         # 각 방향으로 뒤집을 수 있는 경우 찾기
+#         board[i][j] = bw
+#         for di, dj in 8방향
+#             ni, nj = i + di, j + dj
+#             tmp = []        # 다른 색 돌의 좌표 저장
+#             while board[ni][nj] == OP[bw]:
+#                 tmp.append((ni, nj))
+#                 ni, nj = ni + di, nj + dj
+#             if board[ni][nj] == bw:    # 놓은 돌과 같은 색을 만나서 중단된 경우
+#                 for p, q in tmp:
+#                     board[p][q] = bw
+#
+#     bcnt = wcnt = 0
+#     for i in range(N):
+#         for j in range(N):
+#             if board[i][j] == black:
+#                 bcnt += 1
+#             elif board[i][j] == white:
+#                 wcnt += 1
+#     print(f"#{tc} {bcnt} {wcnt}")
+
--- 월말평가와 과목평가/1.과목평가/algo1_광주_3반_이금규.py
+++ 월말평가와 과목평가/1.과목평가/algo1_광주_3반_이금규.py
@@ -1,27 +1,17 @@
-T = int(input())
-
+T = int(input())    # 입력값 받기
 for tc in range(1, T+1):
-    N, M = map(int, input().split()) # N 도화지의 크기 M 도장
+    N, M = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]   # 배열 만들기
 
-    arr = [[0] * N for _ in range(N)]   # 전체 도화지 생성
+    treecount= 0   # 심은 나무의 수
+    maxV = 0    # 가장 비싼 나무의 가격
+    cnt = 0     # 총 나무 심은 비용
+    for i in range(N):
+        for j in range(0, M, 2):
+            cnt += arr[j][i]
+            treecount += 1  # 심은 나무의 수 + 1
+            if maxV <= arr[j][i]:    # 모든 블록 당 가장 큰 수 = 가장 많은 나무의 수
+                maxV = arr[j][i]
+                col = j
 
-
-    for tc2 in range(M):
-        x, y, height, p = map(int, input().split()) # x : 행, y : 열, height : 높이, p : 넓이
-
-
-        for i in range(x, x + p): # 기준점 x에서 너비 p까지
-            for j in range(y, y + height): # 기준점 y에서 높이 height까지
-                if arr[i][j] == 0: # 만약 그 좌표 값이 0이면
-                    arr[i][j] = 1   # 1씩 색칠해라
-
-        cnt = 0
-        for k in range(N):
-            for h in range(N):  # 전체 N * N에서
-                if arr[k][h] == 1:  # 1로 칠해진 곳이 있으면
-                    cnt += 1    # 1씩 카운트
-
-
-    print(f"#{tc} {cnt}")
-
-
+    print(f"#{tc} {cnt} {treecount} {maxV} {col+1}")
