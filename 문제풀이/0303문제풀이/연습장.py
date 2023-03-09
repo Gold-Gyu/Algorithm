74d8ba8b0074a8e05d1db4d52455abecfc6d5e03
Gold-gyu <lkg2097@naver.com>
2023-03-09 23:56:39 +0900

    0309

--- 문제풀이/0303문제풀이/연습장.py
+++ 문제풀이/0303문제풀이/연습장.py
@@ -1,25 +1,10 @@
+
+
 T = int(input())
+for test_case in range(1, T + 1):
+    arr = [list(map(int, input().split())) for _ in range(9)]
 
-di = [0, 1, 1, -1]
-dj = [1, 1, 0, 1]
-
-for tc in range(1, T+1):
-    N = int(input())
-    arr = [list(input()) for _ in range(N)]
-
-    ans = "NO"
-
-    for i in range(N):
-        for j in range(N):
-            if arr[i][j] == "o":
-                for k in range(4):
-                    cnt = 1
-                    for h in range(1, 5):
-                        ni = i + di[k] * h
-                        nj = j + dj[k] * h
-                        if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] == "o":
-                            cnt += 1
-
-                    if cnt == 5:
-                        ans = "YES"
-    print(f"#{tc} {ans}")
+    for i in (0, 3, 6):
+        for j in (0, 3, 6):  # 3*3 격자
+            lst = arr[i][j:j + 3] + arr[i + 1][j:j + 3] + arr[i + 2][j:j + 3]
+            print(lst)
