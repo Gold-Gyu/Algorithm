71fb37c4dcc28aebc3d4a235f5e233f65fd316d8
Gold-gyu <lkg2097@naver.com>
2023-04-10 08:16:07 +0900

    0410

--- 백준/16234 인구이동.py
+++ 백준/16234 인구이동.py
@@ -1,74 +1,62 @@
 from collections import deque
-# 반복은 더 이상 인구이동이 없을 때까지
-# 4방향 탐색
-# 인구차이가 L명 이상 R명 이하면
-# 하루동안 opengate
+import copy
 
-# 방문함수 찍으면서 cnt + 1 == 연합을 이루고 있는 타일의 개수
-# 타일안에
+def is_valid(nr, nc):
+    return 0 <= nr < n and 0 <= nc < n
 
-def is_valid(r, c):
-    return 0 <= r < n and 0 <= c < n
-
-def bfs(i, j):
-    global s
+def bfs(i, j, s):
+    global checkpoint
     global cnt
+    city = copy.deepcopy(arr)
+    v2 = copy.deepcopy(v)
+
     q = deque()
     q.append((i, j))
-
-    s += people[i][j]
+    v2[i][j] = 1
+    s += city[i][j]
 
     while q:
         size = len(q)
-        for z in range(size):
-            i, j = q.popleft()
+        for x in range(size):
+            o, p = q.popleft()
             for k in range(4):
-                nr = i + dr[k]
-                nc = j + dc[k]
-                if is_valid(nr, nc):
-                    if v[nr][nc] == 0:
-                        if l <= abs(people[nr][nc] - people[i][j]) <= r:
-                            s += people[nr][nc]
-                            v[i][j] = 1
-                            v[nr][nc] = 1
-                            q.append((nr, nc))
-                            cnt += 1 # 타일의 개수
+                nr = o + dr[k]
+                nc = p + dc[k]
+                if is_valid(nr, nc) and v2[nr][nc] == 0 and l <= abs(city[o][p] - city[nr][nc]) <= r:
+                    q.append((nr, nc))
+                    v2[nr][nc] = 1
+                    s += city[nr][nc]
+                    cnt += 1
+                    checkpoint += 1
+    change(s, v2)
+
+def change(s, v2):
+
+    ans = s // cnt
+    for i in range(n):
+        for j in range(n):
+            if v2[i][j] == 1:
+                arr[i][j] = ans
+
 
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
-
 n, l, r = map(int, input().split())
-people = [list(map(int, input().split())) for _ in range(n)]
+arr = [list(map(int, input().split())) for _ in range(n)]
 v = [[0] * n for _ in range(n)]
-openday = 0
+count = 0
+cp = 0
+checkpoint = 0
 cnt = 1
-s = 0
-flag = 0
+while cp < n*n:
 
-# 1. 모든 곳을 돌면서 상하좌우를 비교해서 조건에 맞으면 방문함스체크 + 타일개수
-
-while True:
-    end = 0
     for i in range(n):
         for j in range(n):
-            bfs(i, j)   # 연합의 개수, 연합 확인용 방문함수 체크, 연합 명수 총
-            if cnt <= 1:    # 바꿀게없다면
-                end += 1
-            else:   # 바꿀게 있다
-                end = 0
-                cnt2 = 0
-                for o in range(n):
-                    for p in range(n):
-                        if v[o][p] == 1:
-                            people[o][p] = s // cnt
-                            v[o][p] = 0
-                s = 0
-                cnt = 1
-    openday += 1
-    if end == n*n:
-        break
-
-
-
-print(openday)
-
+            bfs(i, j, 0)
+            if checkpoint == 0:
+                cp += 1
+    if cnt >= 2:
+        count += 1
+    cnt = 1
+    checkpoint = 0
+print(count)
