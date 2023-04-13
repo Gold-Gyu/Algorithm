48fed3dc3a1b99e2eaf136be90ac17e608a2cc7d
Gold-gyu <lkg2097@naver.com>
2023-04-14 00:46:49 +0900

    0414

--- 백준/BFS/5427 fire.py
+++ 백준/BFS/5427 fire.py
@@ -4,64 +4,58 @@ from collections import deque
 def is_valid(nr, nc):
     return 0 <= nr < h and 0 <= nc < w
 
-def fire_bfs(fire):
-    q = deque(fire)
+def bfs(fire, o, p):
+    global cnt
+    global flag
 
-    while q:
+    q = deque(fire)
+    # q.append(fire)
+    q2 = deque()
+    q2.append((o, p))
+
+    while True:
         size = len(q)
         for i in range(size):
             r, c = q.popleft()
             for k in range(4):
                 nr = r + dr[k]
                 nc = c + dc[k]
-                if is_valid(nr, nc) and v[nr][nc] == 0 and (field[nr][nc] == "." or field[nr][nc] == "@"):
+                if is_valid(nr, nc) and (field[nr][nc] == "@" or field[nr][nc] == "."):
                     v[nr][nc] = 1
                     field[nr][nc] = "*"
                     q.append((nr, nc))
-        return
 
-def person_bfs(i, j):
-    global cnt
-    global flag
-    global ans
-    q = deque()
-    q.append((i, j))
-
-    while q:
-        size = len(q)
+        size = len(q2)  # 상근
         for i in range(size):
-            x, y = q.popleft()
+            r, c = q2.popleft()
             for k in range(4):
-                nx = x + dr[k]
-                ny = y + dc[k]
-                if v[nx][ny] == 0 and field[nx][ny] == ".":
-                    q.append((nx, ny))
-                    field[nx][ny] = "@"
-                elif not is_valid(nx, ny):
+                nr = r + dr[k]
+                nc = c + dc[k]
+                if 0 > nr or nr >= h or 0 > nc or nc >= w:
                     flag = 1
                     return
-                else:
-                    ans = "IMPOSSIBLE"
+                elif is_valid(nr, nc) and v[nr][nc] == 0 and field[nr][nc] == ".":
+                    field[nr][nc] = "@"
+                    q2.append((nr, nc))
 
-            cnt += 1
+        cnt += 1
+        if len(q2) == 0:    # 더 이상 움직일 곳이 없다
+            flag = 2
             return
-
 
 T = int(input())
 
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
 
-
 for tc in range(1, T+1):
     w, h = map(int, input().split())
     field = [list(input()) for _ in range(h)]
     v = [[0] * w for _ in range(h)]
-    pprint(field)
     fire = []
-    flag = 0
-    cnt = 0
-    ans = ""
+    cnt = 1
+    flag = 1
+    ans = "IMPOSSIBLE"
     for i in range(h):
         for j in range(w):
             if field[i][j] == "@":
@@ -70,13 +64,9 @@ for tc in range(1, T+1):
             elif field[i][j] == "*":
                 v[i][j] = 1
                 fire.append((i, j))
-    # r, c 시작점을 구했음
-    while True:
-        fire_bfs(fire)
-        person_bfs(o, p)
-        if flag == 1:
-            print(cnt)
-            break
-        elif flag == 2:
-            print(ans)
 
+    bfs(fire, o, p)
+    if flag == 1:
+        print(cnt)
+    elif flag == 2:
+        print(ans)
