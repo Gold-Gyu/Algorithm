b9ed35f8a9895647fef84be2af9d7638c64c5524
Gold-gyu <lkg2097@naver.com>
2023-04-18 19:36:56 +0900

    0418

--- 백준/practice.py
+++ 백준/practice.py
@@ -1,19 +1,25 @@
 """
-3 4 5
-S....
-.###.
-.##..
-###.#
+2 3 3
+##E
+##.
+###
 
-#####
-#####
-##.##
-##...
+###
+##.
+#S.
 
-#####
-#####
-#.###
-####E
+3 3 3
+S##
+##.
+##.
+
+..#
+###
+##E
+
+#..
+##.
+##.
 
 0 0 0
 
@@ -21,6 +27,7 @@ S....
 import copy
 from pprint import pprint
 from collections import deque
+
 def is_valid(nr, nc):
     return 0 <= nr < R and 0 <= nc < C
 
@@ -30,72 +37,71 @@ def bfs(stair, r, c):
 
     q = deque()
     q.append((stair, r, c))
-    v[stair*R+r][stair*C+c] = 1
+    v[stair*R+r][c] = 1
 
     while q:
         ans += 1
         size = len(q)
 
         for i in range(size):
-            stair, r, c = q.popleft()
+            stair, x, y = q.popleft()
 
             # 4방향 갈 수 있는 곳
             for k in range(4):
-                nr = r + dr[k]
-                nc = c + dc[k]
+                nr = x + dr[k]
+                nc = y + dc[k]
 
-                if is_valid(nr, nc) and floor[stair][nr][nc] != "#" and v[stair*R + nr][nc] == 0:
+                if is_valid(nr, nc) and (floor[stair][nr][nc] == "." or floor[stair][nr][nc] == "E") and v[stair*R + nr][nc] == 0:
                     q.append((stair, nr, nc))
                     v[stair*R + nr][nc] = 1
+                    if floor[stair][nr][nc] == "E":
+                        return
             # 상하층
             if floor_cnt > 1:
                 if stair == 0:
                     stair = stair + height
                     if floor_cnt > stair:
-                        if floor[stair][r][c] == "." and v[stair*R + r][c] == 0:
-                            q.append((stair, r, c))
-                            v[stair*R + r][c] = 1
-                            stair = stair - height
+                        if is_valid(x, y) and floor[stair][x][y] == "." and v[stair*R + x][y] == 0:
+                            q.append((stair, x, y))
+                            v[stair*R + x][y] = 1
+
                 elif stair > 0:
-                    stair = stair + height
-                    if floor_cnt > stair:
-                        if is_valid(r, c) and floor[stair][r][c] == "." and v[stair * R + r][c] == 0:
-                            q.append((stair, r, c))
-                            v[stair * R + r][c] = 1
-                        elif floor[stair][r][c] == "E":
+                    stairup = stair + height
+                    if floor_cnt > stairup:
+                        if is_valid(x, y) and floor[stairup][x][y] == "." and v[stairup * R + x][y] == 0:
+                            q.append((stairup, x, y))
+                            v[stairup * R + x][y] = 1
+                        elif floor[stairup][x][y] == "E":
                             return
-                        stair = stair - height*2
-                        if is_valid(r, c) and floor[stair][r][c] == "." and v[stair * R + r][c] == 0:
-                            q.append((stair, r, c))
-                            v[stair * R + r][c] = 1
-                            stair = stair - height * 2
-                        elif floor[stair][r][c] == "E":
+                    stairdown = stair - height
+                    if stairdown >= 0:
+                        if is_valid(x, y) and floor[stairdown][x][y] == "." and v[stairdown * R + x][y] == 0:
+                            q.append((stairdown, x, y))
+                            v[stairdown * R + x][y] = 1
+
+                        elif floor[stairdown][x][y] == "E":
                             return
-
-
         if len(q) == 0:
             flag2 = 1
             return
-
 
 
 dr = [1, -1, 0, 0]
 dc = [0, 0, 1, -1]
 height = 1
 
-
 while True:
     ans = 0
     L, R, C = map(int, input().split())
 
-    if L == R == C == 0:
+    if L == 0 and R == 0 and C == 0:
         break
     floor = []
     for _ in range(L):
         floor.append([list(input()) for _ in range(R)])
         blank = input()
     floor_cnt = len(floor)  # 층의 개수
-    v = [[0] * C for _ in range(R * C + floor_cnt)]
+    v = [[0] * C for _ in range(R * C)]
     here = 0    #층 인덱스
     flag = 0
     flag2 = 0
@@ -104,7 +110,7 @@ while True:
         for r in range(R):
             for c in range(C):
                 if floor[stair][r][c] == "S":
-                    v[stair*R + r][c] = 1
+
                     bfs(stair, r, c)
                     flag = 1
                     break
