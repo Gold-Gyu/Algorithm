7ad4f65bd5f83865ca2818e0125eb6d36149a914
goldgyu <lkg2097@naver.com>
2023-04-25 17:58:10 +0900

    0425

--- 백준/Graph/12851 숨바꼭질2.py
+++ 백준/Graph/12851 숨바꼭질2.py
@@ -2,41 +2,38 @@ from collections import deque
 
 
 def bfs(n):
-    global minV
-    global cnt
-    global samething
-    v = [0] * 100001
+
+    global count
+    flag = 0
+
     q = deque()
     q.append(n)
     v[n] = 1
-
     while q:
 
         size = len(q)
-        cnt += 1
+
         for _ in range(size):
             x = q.popleft()
+            if x == m:
+                count += 1
             for nx in (x * 2, x + 1, x - 1):
-                if 0 <= nx < 100001 and v[nx] == 0:
-                    if nx == m:
-                        samething += 1
-                        minV = cnt
+                if 0 <= nx < 100001:
 
-                    elif nx != m:
+                    # 한번도 방문하지 않았거나 방문시간이 같은 경우
+                   if v[nx] == 0 or v[nx] == v[x] + 1:
+                        v[nx] = v[x] + 1
                         q.append(nx)
-                        v[nx] = 1
-        if minV == cnt:
-            return
 
 
 n, m = map(int, input().split())
-minV = 99999
-cnt = 0
-samething = 0
+v = [0] * 100001
+
+count = 0
 if n == m:
     print(0)
     print(1)
 else:
     bfs(n)
-    print(minV)
-    print(samething)
+    print(v[m]-1)
+    print(count)
