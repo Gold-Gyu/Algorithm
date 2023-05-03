3d1b09c87193c9f0eccbd5cc4ef6b36dd195de18
Gold-gyu <lkg2097@naver.com>
2023-05-03 20:21:58 +0900

    0503

---
+++ Vue/3-5과제/q4/src/components/Finaltax.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h2>결정세액 : {{ year_num }} 만원</h2>
+
+    </div>
+  </template>
+  
+  <script>
+  
+  
+  export default {
+    name: 'Finaltax',
+    components: {
+      
+    }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
+  
