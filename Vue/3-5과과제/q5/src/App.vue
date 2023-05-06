5f810c20c56caec7e94ddf5a74814de08b01ab13
Gold-gyu <lkg2097@naver.com>
2023-05-06 10:07:12 +0900

    0504

---
+++ Vue/3-5과과제/q5/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app" class="box">
+    <div class=""><h1>App</h1>
+      <input v-model.number="app_num" type="number">
+      <h3>parentData : {{ number2 }}</h3>
+      <h3>childData : {{ endnum }}</h3></div>
+    <div><AppParent :app_num="app_num" @gotoapp="applyapp" @childapply="endgame"/></div>
+    <div><AppChild :app_num="app_num" @childgo="lastgo"/></div>
+
+    
+  </div>
+</template>
+
+<script>
+import AppParent from "./components/AppParent.vue"
+import AppChild from "./components/AppChild.vue"
+export default {
+  name: 'App',
+  components: {
+    AppParent,
+    AppChild
+  },
+  data() {
+    return {
+      app_num : null,
+      number2 : "",
+      endnum : null
+
+    }
+  },
+  methods :{
+    applyapp(number2) {
+      this.number2 = number2
+    },
+    lastgo(lastnum) {
+      this.lastnum = lastnum
+    },
+    endgame(endnum) {
+      this.endnum = endnum
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.box {
+  border: 1px solid black;
+}
+</style>
