b37fff9ce8632bff6e624f0acdd8038d844b3f2e
Gold-gyu <lkg2097@naver.com>
2023-05-14 20:49:59 +0900

    0514

--- 백준/Graph/맥주마시면서 걷기.py
+++ 백준/Graph/맥주마시면서 걷기.py
@@ -19,13 +19,14 @@ def startwalking(x, y, ex, ey):
 
     q = deque()
     v = [0] * n
+    # 시작점을 큐에 넣어준다.
     q.append((x, y))
-
 
     while q:
         cj, ci = q.popleft()
         if abs(cj - ex) + abs(ci - ey) <= 1000:
-            return "happy"
+            return "happy"  # 맥주를 마시면서 도착할 수 있다면 happy 출
+
         # 미방문 모든 편의점 좌표 : 범위 내인지 확인
         for i in range(n):
             if v[i] == 0: # 방문하지 않은 편의점이라면
@@ -41,6 +42,7 @@ for tc in range(1, T+1):
     n = int(input())
     homeX, homeY = map(int, input().split())
     storelst = []
+
     for store_num in range(n):
         storeX, storeY = map(int, input().split())
         storelst.append((storeX, storeY))
