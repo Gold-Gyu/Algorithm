20072fbefae2d4f0b26bfd99a29bf9fd750b637a
Gold-gyu <lkg2097@naver.com>
2023-07-26 01:44:59 +0900

    0726

--- 백준/Combinatorics/13022 늑대의 올바른 단더.py
+++ 백준/Combinatorics/13022 늑대의 올바른 단더.py
@@ -1,8 +1,36 @@
 n = input()
-print(n)
-
-standard = { "w": 0, "o" : 0, "l" : 0, "f" : 0}
+lst = [0, 0, 0, 0]
 # 각 문자의 개수가 n개가 나란히 나오면 O
 # 문자 두개를 이은것도 올바른단어
 size = len(n)
+flag = 0
 for i in range(size):
+    if n[i] == "w":
+        lst[0] += 1
+        if lst[0] == lst[1] == lst[2] == lst[3]:
+            flag = 1
+            break
+
+    elif n[i] == "o":
+        lst[1] += 1
+        if lst[0] < lst[1] or lst[2] > lst[1] and lst[3] > lst[1]:
+            flag = 1
+            break
+
+    elif n[i] == "l":
+        lst[2] += 1
+        if lst[2] > lst[0] or lst[2] > lst[1] or lst[3] > lst[2]:
+            flag = 1
+            break
+    else:
+        lst[3] += 1
+        if lst[3] > lst[0] or lst[3] > lst[1] or lst[3] > lst[2]:
+            flag = 1
+            break
+
+if flag == 0 and lst[0] == lst[1] == lst[2] == lst[3]:
+    print(1)
+else:
+    print(0)
+
+# wolffwwoollf
--- 백준/practice.py
+++ 백준/practice.py
@@ -1,96 +1,45 @@
-from pprint import pprint
-from collections import deque
+n = input()
 
+standard = {"w": 0, "o": 0, "l": 0, "f": 0}
+lst = [0, 0, 0, 0]
+# 각 문자의 개수가 n개가 나란히 나오면 O
+# 문자 두개를 이은것도 올바른단어
+size = len(n)
 
+flag = 0
+for i in range(size):
+    if n[i] == "w":
+        lst[0] += 1
+        if lst[0] == lst[1] == lst[2] == lst[3]:
+            flag = 1
+            break
+        elif lst[0] - lst[2] >= 2 or lst[0] - lst[3] >= 2:
+            flag = 1
+            break
+    elif n[i] == "o":
+        lst[1] += 1
+        if lst[0] < lst[1] or lst[2] > lst[1] or lst[3] > lst[1]:
+            flag = 1
+            break
 
-def is_valid(nr, nc):
-    return 0 <= nr < n and 0 <= nc < m and arr[nr][nc] != -1
-# dkdkdkdkdkdkdkdkdkdkdkdkdkdkdkdkdk
-
-
-def wind2(x, y):
-    v = [[0] * m for _ in range(n)]
-    d = 0
-
-    while True:
-
-        nx = x + dr[d]
-        ny = y + dc[d]
-        if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == -1:
-            return
-        elif 0 <= nx < n and 0 <= ny < m:
-            v[nx][ny] = arr[nx][ny]
-            if arr[x][y] == -1:
-                arr[nx][ny] = 0
-            else:
-                arr[nx][ny] = v[x][y]
-            x = nx
-            y = ny
+    elif n[i] == "l":
+        lst[2] += 1
+        if lst[2] > lst[0] or lst[2] < lst[3] or lst[1] < lst[2]:
+            flag = 1
+            break
+    else:
+        if lst[0] != 0 and lst[1] != 0 and lst[2] != 0 and lst[0] == lst[1] == lst[2]:
+            lst[3] += 1
         else:
-            d += 1
-            if d == 4:
-                d = 0
+            flag = 1
+            break
 
+if flag == 0 and lst[0] == lst[1] == lst[2] == lst[3]:
+    print(1)
+elif flag == 1:
+    print(0)
+else:
+    print(0)
 
-def bfs():
-    q = deque(dust)
-    while q:
-
-        size = len(q)
-        for _ in range(size):
-            r, c = q.popleft()
-            cnt = 0
-            # 먼지 있는 곳에서
-            # 4방향 탐색
-            for k in range(4):
-                nr = r + dr[k]
-                nc = c + dc[k]
-                if is_valid(nr, nc):
-                    v[nr][nc] += (arr[r][c] // 5)
-                    cnt += 1
-            # 4방향 탐색했으면 자기 자신은 조건대로 깍기
-            # 방문함수에 퍼진 먼지를
-            # 본체 함수에는 부모먼지의 줄어든 양을 나타내서
-            # 더한다.
-            arr[r][c] = arr[r][c] - ((arr[r][c] // 5) * cnt)
-    for i in range(n):
-        for j in range(m):
-            arr[i][j] += v[i][j]
-
-
-dr = [0, 1, 0, -1]
-dc = [1, 0, -1, 0]
-
-n, m, t = map(int, input().split())
-
-arr = [list(map(int, input().split())) for _ in range(n)]
-v = [[0] * m for _ in range(n)]
-dust = []
-cleaning = []
-for T in range(t):
-    for i in range(n):
-        for j in range(m):
-            if arr[i][j] > 0:
-                dust.append((i, j))  # 먼지가 있는 곳들 전부다 집어넣기
-            elif arr[i][j] == -1:
-                cleaning.append((i, j))
-    bfs()  # 확산
-
-    # pprint(arr)
-
-    x, y = cleaning[0]
-    wind1(x, y)
-    x, y = cleaning[1]
-    wind2(x, y)
-    # 바람 부는 것
-    # 위에 것
-    # pprint(arr)
-    # print("=============================")
-    v = [[0] * m for _ in range(n)]
-cnt = 0
-for i in range(n):
-    for j in range(m):
-        if arr[i][j] > 0:
-            cnt += arr[i][j]
-
-print(cnt)
+#wolffwwoollf
+#wowollff
